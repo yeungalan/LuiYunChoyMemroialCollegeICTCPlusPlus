2861cc161590adcd9b0e078c97a105ae4049cb82
SuperUser <yeungbluecat123@gmail.com>
2018-09-11 11:05:57 +0800

    lab1

---
+++ lab1_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	printf("Yeung Ho Lun says Hello to the WHOLE world!\n");
+	printf("    -//-\n");
+    printf("   |    |\n");
+    printf("  . o   < .\n");
+    printf("  .       .\n");
+    printf("   .  _  .\n");
+    printf("   .    .\n");
+    printf("    ....\n");
+	printf("\n........By Yeung Ho Lun;ICP;4020; 2018.......\n");
+	return 0;
+}
