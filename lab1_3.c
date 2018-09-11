c898a91a31cd6bb6d4d930c4331abd4b0708c14a
SuperUser <yeungbluecat123@gmail.com>
2018-09-11 11:21:16 +0800

    Add files via upload

---
+++ lab1_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	printf("Step1: Set A as the first number\n");
+	printf("Step2: Set A as the second number\n");
+	printf("Step3: Sum A and B then save the result in ANS\n");
+	printf("Step4: Print the variable ANS on screen\n");
+	printf("\n........By Yeung Ho Lun;ICP;4020; 2018.......\n");
+	return 0;
+}
