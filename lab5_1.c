20b618322832bb341587a2b32df77460a575b5d7
SuperUser <yeungbluecat123@gmail.com>
2018-10-23 09:17:38 +0800

    Add files via upload

--- lab5_1.c
+++ lab5_1.c
@@ -2,11 +2,11 @@
 int main(){
 	char ch;
 	do{
-		fflush(stdin);
-		printf("Please input a character and then <Enter>, 'q' for exit.");
-		scanf("%c",&ch);
-		printf("The character you typed is [%c]\n",ch);
-	}while (ch != 'q' && ch != 'Q');
-	printf("The program quits, as you want...");
-	return 0;
+		fflush(stdin);														/* flush the stdin cache */
+		printf("Please input a character and then <Enter>, 'q' for exit."); /* print the string */
+		scanf("%c",&ch); 													/* ask user to input some value into ch */
+		printf("The character you typed is [%c]\n",ch); 					/* display the value to user */
+	}while (ch != 'q' && ch != 'Q');  										/* while statment which use for check if character is 'q' or 'Q' */
+	printf("The program quits, as you want...");  							/* print the string */
+	return 0; 
 }
--- lab5_2.c
+++ lab5_2.c
@@ -3,21 +3,23 @@ int main(){
 	int num;
 	int pass = 88;
 	do{
-		fflush(stdin);
-		printf("Guess what the lucky number is (between 1 and 100), then <Enter>:");
-		scanf("%d",&num);
-		if(num < pass && num >= 1){
-			printf("The number you typed is [%d] Too small, guess the lucky number again!\n",num);
-		}else if(num > pass && num <= 100){
-			printf("The number you typed is [%d] Too LARGE, guess the lucky number again!\n",num);
-		}else if(num == pass){
-			printf("OK! This is your LUCKY NUMBER [%d]. Well Done!... QUIT!\n",pass);
+		fflush(stdin);																				/* flush the stdin cache */
+		printf("Guess what the lucky number is (between 1 and 100), then <Enter>:");				/* print string */
+		scanf("%d",&num);																			/* ask user to input string */
+		if(num < pass && num >= 1){ 															    /* if num is bigger than 1 and less than lucky number */
+			printf("The number you typed is [%d] Too small, guess the lucky number again!\n",num); /* print string */
+		}else if(num > pass && num <= 100){														   /* if num is bigger than 1 and less than lucky number */
+			printf("The number you typed is [%d] Too LARGE, guess the lucky number again!\n",num); /* print string */
+		}else if(num == pass){																	   /* if num = lucky number */
+			printf("OK! This is your LUCKY NUMBER [%d]. Well Done!... QUIT!\n",num);			   /* print string */
+			break;																				   /* break the loop*/
+		}else{																					   /* out of range */
+			printf("The input number [%d] is OUT of range... QUIT!\n",num);						   /* print string */
+			break;																				   /* break the loop*/
 		}
 		
 	}while (num >= 1 && num <= 100 && num != pass);
-	if(num != pass){
-		printf("The input number [%d] is OUT of range... QUIT!\n",num);
-	}
+
 	
 	return 0;
 }
