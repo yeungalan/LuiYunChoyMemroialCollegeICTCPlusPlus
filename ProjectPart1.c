3daee968073e940a6f5ccb0d0f5a34c10b34e973
SuperUser <yeungbluecat123@gmail.com>
2018-11-20 16:13:10 +0800

    Update ProjectPart1.c

--- ProjectPart1.c
+++ ProjectPart1.c
@@ -5,56 +5,107 @@
 
 struct Item{
 	int recordnumber;
-	char name;
+	char name[40];
 	int itemnumber;
-	char category;
+	char category[40];
 	int quantity;
 	float weight;
-	char recipient;
-	char finaldestination;
-	char deliverystatus;
+	char recipient[40];
+	char finaldestination[40];
+	char deliverystatus[40];
 };
 
-int chardatatypeschecking(int,int,char[]);
+int chardatatypeschecking(char[]);
 
 int main(){
+	int TempCharArrayCounterForLastThreeItems = 0;
+	int flag = 1,counter[5],IsContinueCharInput = 0;
 	struct Item Arr_Item[50];
-	char inputmessage[255];
-	int lasti = 0 ,lastflag = 0,currentStringPart = 0,i;
-	int IntCase = 0, FloatCase = 0, CharCase = 0;
+	counter[1] = 0;
+	counter[2] = 0;
+	counter[3] = 0;
 	
-	printf("Please enter 1) Record number, 2) Item name, 3) Item number, 40 Category, 5) Quantity 6) Weight 7) Recipient 8) Final Destination, and 9) Delivery status:");
-	gets(inputmessage);
-	while(inputmessage[i] != '\0'){
-		if(isspace(inputmessage[i]) || strlen(inputmessage) == i +1){
-			int flag = chardatatypeschecking(lasti,i,inputmessage);
-			switch(flag){
-				case 1:
-					if(currentStringPart == 0){
-						int StringLocation;
-						for(StringLocation=lasti;StringLocation <= i;StringLocation++){
-							Arr_Item[0].recordnumber = atoi(inputmessage[StringLocation]);
-						}
+	printf("███████████████████████ CCIT4020-ICP-GroupProject ███████████████████████\n");
+	printf("\n HKUSPACE Inventory Management and Record System\n Powered by Yeung Ho Lun(20093247)\n Add new item(s):\n");
+	printf("\n\n\n");	
+	printf("Please enter 1) Record number, 2) Item name, 3) Item number, 4) Category, 5) Quantity 6) Weight 7) Recipient 8) Final Destination, and 9) Delivery status:\n");
+	while(flag){
+		char tmp[20];
+		scanf("%s",tmp);
+	
+		switch(chardatatypeschecking(tmp)){
+			case 1:
+				if(counter[1] == 0){
+					Arr_Item[0].recordnumber = atoi(tmp);
+				}else if(counter[1] == 1){
+					Arr_Item[0].itemnumber = atoi(tmp);	
+				}else if(counter[1] == 2){
+					Arr_Item[0].quantity = atoi(tmp);	
+				}
+				counter[1] += 1;
+				IsContinueCharInput = 0;
+				break;
+			case 2:
+				if(counter[2] == 0){
+					Arr_Item[0].weight = atof(tmp);
+				}
+				counter[2] += 1;
+				IsContinueCharInput = 0;
+				break;
+			case 3:
+				if(IsContinueCharInput == 0){
+					counter[3] += 1;
+				}
+				strcat(tmp," ");
+				if(counter[3] == 1){
+					strcat(Arr_Item[0].name,tmp);
+				}else if(counter[3] == 2){
+					strcat(Arr_Item[0].category,tmp);
+				}else if(counter[3] == 3){
+					if(TempCharArrayCounterForLastThreeItems == 2){
+						counter[3] += 1;
+						TempCharArrayCounterForLastThreeItems = 0;
 					}
-					break;
-				case 2:
-					break;	
-				case 3:
-					break;
-			};
-		lastflag = flag;		
-		lasti = i;
-		currentStringPart = currentStringPart + 1;
-	}
-	i = i + 1;
-	}
+					//printf("%d %s\n",TempCharArrayCounterForLastThreeItems,tmp);
+					strcat(Arr_Item[0].recipient,tmp);
+					TempCharArrayCounterForLastThreeItems += 1;
+				}else if(counter[3] == 4){
+					if(TempCharArrayCounterForLastThreeItems == 2){
+						counter[3] += 1;
+						TempCharArrayCounterForLastThreeItems = 0;
+					}
+					//printf("%d %s\n",TempCharArrayCounterForLastThreeItems,tmp);
+					strcat(Arr_Item[0].finaldestination,tmp);
+					TempCharArrayCounterForLastThreeItems += 1;
+				}else if(counter[3] == 5){
+					strcat(Arr_Item[0].deliverystatus,tmp);
+					flag = 0;
+				}
+				//printf("%d %s\n",counter[3],tmp);
+				IsContinueCharInput = 1;
+				break;
+		}
+	};
+
+	/* yea this is the fucking debug unit, do not remove this fucking part */
+	printf(" \n \nThe input has been recognized by system, please check the input data integrity at here: \n");
+	printf("Record number :		%d \n",Arr_Item[0].recordnumber);
+	printf("Name : 			%s \n",Arr_Item[0].name);
+	printf("Item Number :		%d \n",Arr_Item[0].itemnumber);
+	printf("Category : 		%s \n",Arr_Item[0].category);
+	printf("Quantity : 		%d \n",Arr_Item[0].quantity);
+	printf("Weight : 		%f \n",Arr_Item[0].weight);
+	printf("Recipient : 		%s \n",Arr_Item[0].recipient);
+	printf("Final destination : 	%s \n",Arr_Item[0].finaldestination);
+	printf("Delivery status : 	%s \n",Arr_Item[0].deliverystatus);
 	
-	printf("%d %s %d %s %d %f",Arr_Item[0].recordnumber,Arr_Item[0].name,Arr_Item[0].itemnumber,Arr_Item[0].category,Arr_Item[0].quantity,Arr_Item[0].weight);
+
 }
 
-int chardatatypeschecking(int firsti,int lasti,char chararray[]){
+
+int chardatatypeschecking(char chararray[]){
 	int i = 0,AlphaCounter = 0,DigitCounter = 0,DotCounter = 0,SpaceCounter = 0;
-	for(i=firsti;i<=lasti;i++){
+	for(i=0;i<=strlen(chararray);i++){
 		if(!isspace(chararray[i])){
 			if(isalpha(chararray[i])){
 				AlphaCounter = AlphaCounter + 1;
@@ -67,12 +118,11 @@ int chardatatypeschecking(int firsti,int lasti,char chararray[]){
 		}else{
 			SpaceCounter = SpaceCounter + 1;
 		}
-		//printf("%c / S%d / D%d / A%d / Digit%d\n",chararray[i],SpaceCounter,DotCounter,AlphaCounter,DigitCounter);
 	}
-	
-	if(DigitCounter == (lasti - firsti + 1) - SpaceCounter){
+	//printf("%s / S%d / D%d / A%d / Digit%d\n",chararray,SpaceCounter,DotCounter,AlphaCounter,DigitCounter);
+	if(DigitCounter == strlen(chararray)){
 		return 1; //int
-	}else if(DotCounter == 1 && DigitCounter == (lasti - firsti + 1) - 1 - SpaceCounter){
+	}else if(DotCounter == 1 && DigitCounter == strlen(chararray) - 1){
 		return 2; //float
 	}else{
 		return 3; //char
